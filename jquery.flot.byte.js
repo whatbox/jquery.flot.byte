1cdbaf8839f6880f854303486c07dba65ca9cd17
Anthony Ryan <anthonyryan1@gmail.com>
2013-08-02 22:45:06 -0400

    Stricter jshint rules
    Minor cleanup for compliance with jshint
    A couple extra comments for clarity
    Removed excessive variables

--- jquery.flot.byte.js
+++ jquery.flot.byte.js
@@ -9,66 +9,60 @@
 	}
 
 	function init(plot) {
-		plot.hooks.processDatapoints.push(function (plot, series, datapoints) {
+		plot.hooks.processDatapoints.push(function (plot) {
 			$.each(plot.getAxes(), function(axisName, axis) {
 				var opts = axis.options;
 				if (opts.mode === "byte" || opts.mode === "byteRate") {
 					axis.tickGenerator = function (axis) {
+						var returnTicks = [],
+							tickSize = 2,
+							delta = axis.delta,
+							steps = 0,
+							tickMin = 0,
+							tickVal,
+							tickCount = 0;
+
 						//Set the reference for the formatter
 						if (opts.mode === "byteRate") {
 							axis.rate = true;
 						}
 
 						//Enforce maximum tick Decimals
-						if (typeof opts.tickDecimals === 'number') {
+						if (typeof opts.tickDecimals === "number") {
 							axis.tickDecimals = opts.tickDecimals;
 						} else {
 							axis.tickDecimals = 2;
 						}
 
-						//Determine the scales
-						var delta = axis.delta,
-							size,
-							steps = 0;
-
+						//Count the steps
 						while (Math.abs(delta) >= 1024) {
 							steps++;
 							delta /= 1024;
 						}
 
-						var b2 = 2;
-						while (b2 <= 1024) {
-							if (delta <= b2) {
-								size = b2;
+						//Set the tick size relative to the remaining delta
+						while (tickSize <= 1024) {
+							if (delta <= tickSize) {
 								break;
 							}
-
-							b2 *= 2;
+							tickSize *= 2;
 						}
 
-						axis.tickSize = size * Math.pow(1024,steps);
-
-						if (typeof opts.minTickSize !== 'undefined' && size < opts.minTickSize) {
+						//Tell flot the tickSize we've calculated
+						if (typeof opts.minTickSize !== "undefined" && tickSize < opts.minTickSize) {
 							axis.tickSize = opts.minTickSize;
+						} else {
+							axis.tickSize = tickSize * Math.pow(1024,steps);
 						}
 
 						//Calculate the new ticks
-						var ticks = [],
-							start = floorInBase(axis.min, axis.tickSize),
-							i = 0,
-							val,
-							prev;
-
-						//Write new ticks
+						tickMin = floorInBase(axis.min, axis.tickSize);
 						do {
-							prev = val;
-							val = start + i * axis.tickSize;
-							ticks.push(val);
-							++i;
-						} while (val < axis.max && val !== prev);
+							tickVal = tickMin + (tickCount++) * axis.tickSize;
+							returnTicks.push(tickVal);
+						} while (tickVal < axis.max);
 
-
-						return ticks;
+						return returnTicks;
 					};
 
 					axis.tickFormatter = function(size, axis) {
@@ -81,20 +75,20 @@
 
 
 						switch (steps) {
-							case 0: ext = ' B';  break;
-							case 1: ext = ' KiB'; break;
-							case 2: ext = ' MiB'; break;
-							case 3: ext = ' GiB'; break;
-							case 4: ext = ' TiB'; break;
-							case 5: ext = ' PiB'; break;
-							case 6: ext = ' EiB'; break;
-							case 7: ext = ' ZiB'; break;
-							case 8: ext = ' YiB'; break;
+							case 0: ext = " B";  break;
+							case 1: ext = " KiB"; break;
+							case 2: ext = " MiB"; break;
+							case 3: ext = " GiB"; break;
+							case 4: ext = " TiB"; break;
+							case 5: ext = " PiB"; break;
+							case 6: ext = " EiB"; break;
+							case 7: ext = " ZiB"; break;
+							case 8: ext = " YiB"; break;
 						}
 
 
 						if (typeof axis.rate !== "undefined") {
-							ext += '/s';
+							ext += "/s";
 						}
 											
 						return (size.toFixed(axis.tickDecimals) + ext);
@@ -107,7 +101,7 @@
 	$.plot.plugins.push({
 		init: init,
 		options: options,
-		name: 'byte',
-		version: '0.1.0'
+		name: "byte",
+		version: "0.1"
 	});
 })(jQuery);
